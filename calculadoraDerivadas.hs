8ec1c592e001745097247961e9b25cbf6319c920
Mauricio Romero Laino <Romelai28@gmail.com>
2024-06-20 21:10:53 -0300

    función de formato

--- calculadoraDerivadas.hs
+++ calculadoraDerivadas.hs
@@ -1,20 +1,22 @@
+-- Pendiente, considerar la posibilidad de pedir que los casos marcados como problematicos puedan pedir que el fijo reduzca a una constante.
+
 data Funcion a = X
                 | Cte a
                 | Suma (Funcion a) (Funcion a)
                 | Prod (Funcion a) (Funcion a)
                 | Frac (Funcion a) (Funcion a)
                 | LogNat (Funcion a)
-                | LogBase (Funcion a) (Funcion a)
+                | LogBase a (Funcion a)             -- PROBLEMATICO
                 | Exp_e (Funcion a)  -- e^a
-                | Potencia_base_fija a (Funcion a)
-                | Potencia (Funcion a) a
+                | Potencia_base_fija a (Funcion a)  -- PROBLEMATICO
+                | Potencia (Funcion a) a            -- PROBLEMATICO
                 | Sin (Funcion a)
                 | Cos (Funcion a)
                 | Tan (Funcion a)
                 deriving (Eq,Show,Read)
 
 
-foldFunc :: b -> (a -> b) -> (b -> b -> b) -> (b -> b -> b) -> (b -> b -> b) -> (b -> b) -> (b -> b -> b) -> (b -> b)-> (a -> b-> b) -> (b -> a -> b) -> (b -> b) -> (b -> b) -> (b -> b) -> Funcion a -> b
+foldFunc :: b -> (a -> b) -> (b -> b -> b) -> (b -> b -> b) -> (b -> b -> b) -> (b -> b) -> (a -> b -> b) -> (b -> b)-> (a -> b-> b) -> (b -> a -> b) -> (b -> b) -> (b -> b) -> (b -> b) -> Funcion a -> b
 foldFunc cX cCte cSuma cProd cFrac cLogNat cLogBase cExp_e cPotencia_base_fija cPotencia cSin cCos cTan func = case func of
     X -> cX
     Cte k -> cCte k
@@ -22,7 +24,7 @@ foldFunc cX cCte cSuma cProd cFrac cLogNat cLogBase cExp_e cPotencia_base_fija c
     Prod f g -> cProd (rec f) (rec g)
     Frac f g -> cFrac (rec f) (rec g)
     LogNat f -> cLogNat (rec f)
-    LogBase base f -> cLogBase (rec base) (rec f)
+    LogBase base f -> cLogBase base (rec f)
     Exp_e f -> cExp_e (rec f)
     Potencia_base_fija base f -> cPotencia_base_fija base (rec f)
     Potencia f n -> cPotencia (rec f) n
@@ -40,6 +42,7 @@ negativo :: (Floating a, Eq a) => Funcion a -> Funcion a
 negativo = Prod (Cte (-1))
 
 
+-- Se pued escribir como un fold pero no era claro.
 derivar :: (Floating a, Eq a) => Funcion a -> Funcion a
 derivar func = case func of
     X            -> Cte 1
@@ -47,10 +50,10 @@ derivar func = case func of
     Suma f g     -> Suma (derivar f) (derivar g)
     Prod f g     -> Suma (Prod f (derivar g)) (Prod (derivar f) g)
     Frac f g     -> Frac (Suma (Prod (derivar f) g) (negativo(Prod f (derivar g))))
-                        (Prod g g)   -- g^2
+                        (Prod g g)
     LogNat f     -> Frac (derivar f) (f)
     LogBase b f  -> Frac (derivar f)
-                        (Prod f (LogNat b))
+                        (Prod f (LogNat (Cte b)))
     Exp_e f      -> Prod (Exp_e f) (derivar f)
 
     Potencia_base_fija b f -> Prod (Prod (Potencia_base_fija b f) (LogNat (Cte b))) (derivar f)
@@ -61,3 +64,24 @@ derivar func = case func of
     Cos f        -> Prod (negativo (Sin f)) (derivar f)
     Tan f        -> Prod (Suma (Cte 1) (Prod (Tan f) (Tan f))) (derivar f)
 
+
+-- Devuelve una string de la función más amigable de leer. (Genera parentesis redundates)
+formato :: (Show a) => Funcion a -> String
+formato func = case func of
+    X -> "X"
+    Cte k -> show k
+    Suma f g -> "(" ++ formato f ++ "+" ++ formato g ++ ")"
+    Prod f g -> "(" ++ formato f ++ "*" ++ formato g ++ ")"
+    Frac f g -> "(" ++ formato f ++ "/" ++ formato g ++ ")"
+    LogNat f -> "ln(" ++ formato f ++ ")"
+    LogBase b f -> "log_" ++ show b ++ "_(" ++ formato f ++ ")"
+    Exp_e f -> "e^{" ++ formato f ++ "}"
+    Potencia_base_fija b f -> show b ++ "^{" ++ formato f ++ "}"
+    Potencia f n -> formato f ++ "^{" ++ show n ++ "}"  -- ¿Hace falta agregar parentesis a la f?
+    Sin f -> "sin(" ++ formato f ++ ")"
+    Cos f -> "cos(" ++ formato f ++ ")"
+    Tan f -> "tan(" ++ formato f ++ ")"
+
+
+-- Pendiente: simplificar.
+-- simplificar :: (Floating a, Eq a) => Funcion a -> Funcion a
